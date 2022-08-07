7b7bc81d680265c281367ed0dd29a5121ac9a03f
이경호 <rud0503@gmail.com>
2022-08-07 17:53:19 +0900

    add detail path and namespace

--- practice/pybo/urls.py
+++ practice/pybo/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 
 from . import views
 
-urlpatterns=  {
-    path('', views.index)
-}
+app_name = 'pybo'   #네임스페이스 - 각가의 앱이 관리하는 독립된 이름 공간/ 중복된 URL 별칭 사용이 가능하도록 함
+
+urlpatterns=  [ 
+    path('', views.index, name='index'),    #별칭 부여
+    path('<int:question_id>/', views.detail, name='detail'),
+]
