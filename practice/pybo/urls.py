318923f4526105f96b6dedfcabec11d20bd7a758
이경호 <rud0503@gmail.com>
2022-08-07 18:23:05 +0900

    add answer_create path

--- practice/pybo/urls.py
+++ practice/pybo/urls.py
@@ -7,4 +7,5 @@ app_name = 'pybo'   #네임스페이스 - 각가의 앱이 관리하는 독립�
 urlpatterns=  [ 
     path('', views.index, name='index'),    #별칭 부여
     path('<int:question_id>/', views.detail, name='detail'),
+    path('answer/create/<int:question_id>/', views.answer_create, name='answer_create'),
 ]
